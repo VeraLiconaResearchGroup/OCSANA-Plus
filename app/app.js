3cf2fe24765db6f3160f2579f1cf749c51c25dae
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-10-17 23:03:20 -0400

    restructured the directory. updated a video

--- app/app.js
+++ app/app.js
@@ -1,6 +1,26 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+  , app = express()
+  , doT = require('express-dot')
+  , pub = __dirname+'/public'
+  , view = __dirname+'/views';
 
-app.use(express.static(__dirname));
+//app.use(express.static(__dirname));
+app.set('views', __dirname+'/views');
+app.set('view engine', 'dot');
+app.engine('html', doT.__express);
+app.use('/css',express.static(__dirname+'/public/css'));
+app.use('/images',express.static(__dirname+'/public/images'));
+app.use('/js',express.static(__dirname+'/public/js'));
+app.use('/fonts',express.static(__dirname+'/public/fonts'));
+app.use('/resources',express.static(__dirname+'/public/resources'));
+
+app.get('/', function(req, res){
+	var templateData = {title: "Home | AlgoRun", home_nav: "class='active'"};
+	res.render('index.html', templateData);
+});
+app.get('/getting-started', function(req, res){
+	var templateData = {title: "Getting Started | AlgoRun", getting_started_nav: "class='active'"};
+	res.render('getting-started.html', templateData);
+});
 
 var server = app.listen(31331);
