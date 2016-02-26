e06ff7b7439eda2ad459beb4f55e3733018e8cce
Abdelrahman <abdelrahman.hosny@hotmail.com>
2016-02-25 22:09:24 -0500

    updates documentation files

--- public/js/documentation.js
+++ public/js/documentation.js
@@ -2,17 +2,11 @@ $('#doc-1').click(function(){
     $('#doc-1').attr("class","doc-side-nav doc-active");
     $('#doc-2').attr("class","doc-side-nav");
     $('#doc-3').attr("class","doc-side-nav");
-    $('#doc-pdf').attr("src", "resources/packaging-software-1.2.pdf");
-});
-$('#doc-2').click(function(){
-    $('#doc-2').attr("class","doc-side-nav doc-active");
-    $('#doc-1').attr("class","doc-side-nav");
-    $('#doc-3').attr("class","doc-side-nav");
-    $('#doc-pdf').attr("src", "resources/packaging-software-1.3.pdf");
+    $('#doc-pdf').attr("src", "resources/packaging-software.pdf");
 });
 $('#doc-3').click(function(){
     $('#doc-3').attr("class","doc-side-nav doc-active");
     $('#doc-1').attr("class","doc-side-nav");
     $('#doc-2').attr("class","doc-side-nav");
-    $('#doc-pdf').attr("src", "resources/packaging-bowtie-software.pdf");
+    $('#doc-pdf').attr("src", "resources/packaging-software-examples.pdf");
 });
