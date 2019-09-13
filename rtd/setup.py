25ab531282d3ada835932e8ba791dcd5008b9f63
Lauren P Marazzi <laurenmarazzi@gmail.com>
2019-09-13 15:16:56 -0400

    adding rtd

---
+++ rtd/setup.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""`sphinx_rtd_theme` lives on `Github`_.
+
+.. _github: https://github.com/rtfd/sphinx_rtd_theme
+
+"""
+
+import subprocess
+import distutils.cmd
+from io import open
+from setuptools import setup
+
+
+class UpdateTranslationsCommand(distutils.cmd.Command):
+
+    description = "Run all localization commands"
+
+    user_options = []
+    sub_commands = [
+        ('extract_messages', None),
+        ('update_catalog', None),
+        ('transifex', None),
+        ('compile_catalog', None),
+    ]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for cmd_name in self.get_sub_commands():
+            self.run_command(cmd_name)
+
+
+class TransifexCommand(distutils.cmd.Command):
+
+    description = "Update translation files through Transifex"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        subprocess.run(['tx', 'push', '--source'])
+        subprocess.run(['tx', 'pull'])
+
+
+setup(
+    name='sphinx_rtd_theme',
+    version='0.4.3.dev0',
+    url='https://github.com/rtfd/sphinx_rtd_theme/',
+    license='MIT',
+    author='Dave Snider, Read the Docs, Inc. & contributors',
+    author_email='dev@readthedocs.org',
+    description='Read the Docs theme for Sphinx',
+    long_description=open('README.rst', encoding='utf-8').read(),
+    cmdclass={
+        'update_translations': UpdateTranslationsCommand,
+        'transifex': TransifexCommand,
+    },
+    zip_safe=False,
+    packages=['sphinx_rtd_theme'],
+    package_data={'sphinx_rtd_theme': [
+        'theme.conf',
+        '*.html',
+        'static/css/*.css',
+        'static/js/*.js',
+        'static/fonts/*.*'
+    ]},
+    include_package_data=True,
+    # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
+    entry_points = {
+        'sphinx.html_themes': [
+            'sphinx_rtd_theme = sphinx_rtd_theme',
+        ]
+    },
+    install_requires=[
+       'sphinx'
+    ],
+    extras_require={
+        'dev': [
+            'transifex-client',
+            'sphinxcontrib-httpdomain',
+        ],
+    },
+    classifiers=[
+        'Framework :: Sphinx',
+        'Framework :: Sphinx :: Theme',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+)
