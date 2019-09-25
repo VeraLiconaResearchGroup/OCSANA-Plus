4030536fa63bc805e9a957d4f8cde7eb960afae5
Lauren P Marazzi <laurenmarazzi@gmail.com>
2019-09-25 15:49:01 -0400

    building rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,7 +84,10 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'titles_only': False}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
---
+++ docs/source/tutorials.rst
@@ -0,0 +1,6 @@
+=====
+Tutorials
+=====
+
+
+jioos
